72f20e7e4e960bffcf31b30c3ff652564b76a564
Kevinpacker <kevinpackerdesign@gmail.com>
2022-01-24 17:16:40 -0800

    4.8 serviceworker

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
+import { ErrorAlert } from "./Alert";
+import "./NumberOfEvents.css";
 
 class NumberOfEvents extends Component {
   state = {
     EventsTotal: 32,
+    errorText: "",
   };
 
   handleInputChanged = (event) => {
     const number = event.target.value;
-
-    this.setState({
-      EventsTotal: number,
-    });
+    if (number < 1 || number > 32) {
+      this.setState({
+        EventsTotal: 1,
+        errorText: "Please pick a number between 1 and 32",
+      });
+    } else {
+      this.setState({
+        EventsTotal: number,
+        errorText: "",
+      });
+    }
 
     this.props.updateTotal(number);
     console.log(number);
@@ -19,12 +29,13 @@ class NumberOfEvents extends Component {
   render() {
     return (
       <div className="NumberOfEvents">
-        <p>Number of Events Shown</p>
+        <p className="numbertext">Number of Events Shown</p>
         <input
           value={this.props.numberOfEvents}
           className="newEventsTotal"
           onChange={this.handleInputChanged}
         />
+        <ErrorAlert text={this.state.errorText} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+// import * as serviceWorker from "./service-worker";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
+import reportWebVitals from "./reportWebVitals";
+
+import * as atatus from "atatus-spa";
+atatus.config("c44762ed63c34beeb43aa6911a887a08").install();
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
+// serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
